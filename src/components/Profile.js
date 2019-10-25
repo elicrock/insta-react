3e2f1b4f25349498c3993dab779880ca7e923940
Paul <paul.ivanov1990@gmail.com>
2019-10-25 15:13:17 +0300

    Homework 3

---
+++ src/components/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import User from './User';
+import Palette from './Palette';
+
+const Profile = () => {
+  return (
+    <div className="container profile">
+      <User 
+        src="https://ksassets.timeincuk.net/wp/uploads/sites/46/2017/03/Priti-Patel-re-sized.jpg"
+        alt="jessy_john"
+        name="jessy_john"/>
+      <Palette/>
+    </div>
+  )
+}
+export default Profile;
